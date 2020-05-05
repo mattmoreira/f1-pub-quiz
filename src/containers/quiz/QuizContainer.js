6d9694448aa1e058a84073b05d3f548b264cea82
Matheus Moreira <mattelias@live.com>
2020-05-05 22:44:15 +0200

    Add react spring animation to the elements

--- src/containers/quiz/QuizContainer.js
+++ src/containers/quiz/QuizContainer.js
@@ -1,13 +1,22 @@
 import styles from 'styles/quiz.module.css'
 
 import React from 'react'
+import Color from 'color'
+
+import { useSpring, animated } from 'react-spring'
 
 import QuizLogo from './QuizLogo'
 
 export default function QuizContainer() {
+  const containerStyle = useSpring({
+    delay: 1900,
+    from: { background: Color('#242333').alpha(0).string() },
+    to: { background: '#242333' }
+  })
+
   return (
-    <section className={styles.container}>
+    <animated.section className={styles.container} style={containerStyle}>
       <QuizLogo />
-    </section>
+    </animated.section>
   )
 }
--- src/containers/quiz/QuizLogo.js
+++ src/containers/quiz/QuizLogo.js
@@ -1,15 +1,53 @@
 import styles from 'styles/quiz.module.css'
 
-import React from 'react'
+import React, { useRef } from 'react'
+import { useSpring, useChain, animated } from 'react-spring'
 
 import F1Logo from 'svgs/F1Logo'
 
-export default function QuizLogo() {
-  return (
-    <div>
-      <F1Logo className={styles.formula_logo} />
+function useLogoAnimation() {
+  const logoStyle = useSpring({
+    from: { width: '950px' },
+    to: { width: '380px', fill: '#2F2D3D' },
+    delay: 500
+  })
 
-      <h1 className={styles.title}>Pub Quiz</h1>
+  const titleStyle = useSpring({
+    delay: 1000,
+    from: {
+      height: 0,
+      overflow: 'hidden',
+      marginTop: '0px',
+      opacity: 0,
+      color: '#fff'
+    },
+    to: async next => {
+      await next({ opacity: 1, height: 'auto', marginTop: '50px' })
+      await new Promise(resolve => setTimeout(resolve, 150))
+      await next({ color: '#2F2D3D' })
+    }
+  })
+
+  // useChain([logoAnimationRef, titleAnimationRef], [1, 1.5])
+
+  return {
+    logo: { style: logoStyle },
+    title: { style: titleStyle }
+  }
+}
+
+export default function QuizLogo() {
+  const { title: titleAnimation, logo: logoAnimation } = useLogoAnimation()
+
+  console.log('titleAnimation', titleAnimation)
+
+  return (
+    <div className={styles.quiz_logo}>
+      <F1Logo className={styles.formula_logo} {...logoAnimation} />
+
+      <animated.h1 className={styles.title} style={titleAnimation.style}>
+        <span ref={titleAnimation.ref}>Pub Quiz</span>
+      </animated.h1>
     </div>
   )
 }
