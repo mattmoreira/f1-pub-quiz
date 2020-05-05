67ba0d87b3bfdfe676fca31df8883e33cb48c4b7
Matheus Moreira <mattelias@live.com>
2020-05-05 16:39:53 +0200

    remove boilerplate and add logo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/app.css'
+
+import React from 'react'
+
+import F1Logo from './svgs/F1Logo'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main className="app">
+      <F1Logo />
+    </main>
+  )
 }
 
-export default App;
+export default App
