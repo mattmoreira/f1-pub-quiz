d8a678abeda60bbfa9c96f044a55a78e4ea1ea38
Matheus Moreira <mattelias@live.com>
2020-05-06 22:18:33 +0200

    Improved quiz logo animation

--- src/containers/quiz/QuizLogo.js
+++ src/containers/quiz/QuizLogo.js
@@ -8,7 +8,11 @@ import F1Logo from 'svgs/F1Logo'
 function useLogoAnimation() {
   const logoStyle = useSpring({
     from: { width: '950px' },
-    to: { width: '380px', fill: '#2F2D3D' },
+    to: async next => {
+      await next({ width: '380px' })
+      await new Promise(resolve => setTimeout(resolve, 500))
+      await next({ fill: '#2F2D3D', width: '310px' })
+    },
     delay: 500
   })
 
@@ -19,12 +23,12 @@ function useLogoAnimation() {
       overflow: 'hidden',
       marginTop: '0px',
       opacity: 0,
-      color: '#fff'
+      color: '#fff',
+      fontSize: '98px'
     },
     to: async next => {
       await next({ opacity: 1, height: 'auto', marginTop: '50px' })
-      await new Promise(resolve => setTimeout(resolve, 150))
-      await next({ color: '#2F2D3D' })
+      await next({ color: '#2F2D3D', marginTop: '22.5px', fontSize: '80px' })
     }
   })
 
@@ -37,13 +41,11 @@ function useLogoAnimation() {
 export default function QuizLogo() {
   const { title: titleAnimation, logo: logoAnimation } = useLogoAnimation()
 
-  console.log('titleAnimation', titleAnimation)
-
   return (
     <div className={styles.quiz_logo}>
       <F1Logo className={styles.formula_logo} {...logoAnimation} />
 
-      <animated.h1 className={styles.title} style={titleAnimation.style}>
+      <animated.h1 className={styles.title} {...titleAnimation}>
         <span ref={titleAnimation.ref}>Pub Quiz</span>
       </animated.h1>
     </div>
