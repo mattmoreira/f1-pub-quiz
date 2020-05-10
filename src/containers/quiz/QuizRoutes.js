292b14e29835c9aa1ab4b3e8241ba4f0ee2dff89
Matheus Moreira <mattelias@live.com>
2020-05-10 17:51:01 +0200

    Add leaderboard screen

--- src/containers/quiz/QuizRoutes.js
+++ src/containers/quiz/QuizRoutes.js
@@ -4,9 +4,11 @@ import { Route } from 'react-router'
 import QuizTeamContainer from './team/QuizTeamContainer'
 import QuizQuestionContainer from './question/QuizQuestionContainer'
 import QuizStartContainer from './QuizStartContainer'
+import QuizLeaderboardContainer from './leaderboard/QuizLeaderboardContainer'
 
 const QuizRoutes = () => (
   <>
+    <Route path="/leaderboard" component={QuizLeaderboardContainer} />
     <Route path="/question/:id" component={QuizQuestionContainer} />
     <Route path="/team" component={QuizTeamContainer} />
     <Route exact path="/" component={QuizStartContainer} />
---
+++ src/containers/quiz/leaderboard/QuizLeaderboardContainer.js
@@ -0,0 +1,27 @@
+import styles from './quiz_leaderboard.module.css'
+
+import React from 'react'
+
+function QuizLeaderboardItem({ teamName, points }) {
+  return (
+    <li className={styles.list_item}>
+      <div className={styles.list_item_content}>
+        <h3 className={styles.list_item_team_name}>{teamName}</h3>
+        <p className={styles.list_item_points}>{points} points</p>
+      </div>
+    </li>
+  )
+}
+
+export default function QuizLeaderboardContainer(props) {
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.subtitle}>Leaderboard</h2>
+      <ol className={styles.list}>
+        <QuizLeaderboardItem teamName="Scuderia Ferrari" points={3} />
+        <QuizLeaderboardItem teamName="Scuderia Toro Rosso" points={18} />
+        <QuizLeaderboardItem teamName="Renault F1 Team" points={10} />
+      </ol>
+    </div>
+  )
+}
