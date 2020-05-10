56f4be7f6437b01d86cc32f53d053ba5d07a7a76
Matheus Moreira <mattelias@live.com>
2020-05-10 14:59:39 +0200

    Move quiz logo to shared folder

--- src/containers/quiz/QuizStartContainer.js
+++ src/containers/quiz/QuizStartContainer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import QuizLogo from './QuizLogo'
+import QuizLogo from './shared/QuizLogo'
 import { useHistory } from 'react-router'
 
 export default function QuizStartContainer() {
--- src/containers/quiz/team/QuizTeamContainer.js
+++ src/containers/quiz/team/QuizTeamContainer.js
@@ -6,7 +6,7 @@ import Input from 'components/input/Input'
 import Label from 'components/label/Label'
 import Button from 'components/button/Button'
 
-import QuizLogo from '../QuizLogo'
+import QuizLogo from '../shared/QuizLogo'
 
 function QuizTeamContainer() {
   return (
