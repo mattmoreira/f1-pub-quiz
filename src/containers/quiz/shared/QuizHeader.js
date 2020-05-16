377aadcc26b92ab582b5657ec4745df90bb57c9a
Matheus Moreira <mattelias@live.com>
2020-05-16 18:38:13 +0200

    Add progress bar to quiz header

--- src/containers/quiz/shared/QuizHeader.js
+++ src/containers/quiz/shared/QuizHeader.js
@@ -4,11 +4,17 @@ import QuizNumber from './QuizNumber'
 
 import React from 'react'
 
-export default function QuizHeader({ title, number }) {
+export default function QuizHeader({ title, number, progress }) {
   return (
     <header className={styles.header}>
       {number && <QuizNumber>{number}</QuizNumber>}
       {title && <h1 className={styles.title}>{title}</h1>}
+
+      {progress && (
+        <div
+          className={styles.progress}
+          style={{ width: `${progress}%` }}></div>
+      )}
     </header>
   )
 }
